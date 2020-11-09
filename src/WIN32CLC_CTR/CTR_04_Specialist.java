d6e052f76b8bd25346470905d9c9b00ec80894a7
cesarFreitas <cesar.freitas.dev@gmail.com>
2020-11-09 12:16:21 -0500

    agregando als clases ctr service y specialist

---
+++ src/WIN32CLC_CTR/CTR_04_Specialist.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WIN32CLC_CTR;
+
+/**
+ *
+ * @author Cesar
+ */
+public class CTR_04_Specialist {
+    
+}
