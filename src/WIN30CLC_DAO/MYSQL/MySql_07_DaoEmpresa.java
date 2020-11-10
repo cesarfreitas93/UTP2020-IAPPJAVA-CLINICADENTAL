e585eaae9ce1313b8eee88b8fe0001280ca22f4c
Carlos Jimenez Gomez <Carlos Jimenez Gomez@14.14.6.169>
2020-11-09 22:00:30 -0500

    agregando dao ubigeo  y empresa y jar

--- src/WIN30CLC_DAO/MYSQL/MySql_07_DaoEmpresa.java
+++ src/WIN30CLC_DAO/MYSQL/MySql_07_DaoEmpresa.java
@@ -8,37 +8,202 @@ package WIN30CLC_DAO.MYSQL;
 import WIN30CLC_DAO.DaoException;
 import WIN30CLC_DAO.Dao_07_Empresa;
 import WIN31CLC_DTO.Empresa;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
  *
  * @author Carlos Jimenez Gomez
  */
-public class MySql_07_DaoEmpresa implements Dao_07_Empresa{
+public class MySql_07_DaoEmpresa implements Dao_07_Empresa {
+
+    private Connection conn;
+
+    public MySql_07_DaoEmpresa(Connection conn) {
+        this.conn = conn;
+    }
+
+    final String INSERT = "insert into empresa (name , address, ruc, ubigeo, phone, email, logo)values(?,?, ?, ?,?,?,?);";
+    final String FINDALL = "select id, name , address, ruc, ubigeo, phone, email, logo  from empresa";
+    final String FINDBYID = "select id, name , address, ruc, ubigeo, phone, email, logo  from empresa where id = ?";
+    final String UPDATE = "update  name= ? , address= ?, ruc= ?, ubigeo= ?, phone= ?, email= ?, logo  = ? from empresa where id = ?";
 
     @Override
     public void rlInsert(Empresa entity) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            Calendar calendar = Calendar.getInstance();
+            java.util.Date currentTime = calendar.getTime();
+            long time = currentTime.getTime();
+
+            pst = (PreparedStatement) conn.prepareStatement(INSERT, new String[]{"id"});
+            pst.setString(1, entity.getName());
+            pst.setString(2, entity.getAddress());
+            pst.setString(3, entity.getRuc());
+            pst.setString(4, entity.getUbigeo());
+            pst.setString(5, entity.getPhone());
+            pst.setString(6, entity.getEmail());
+            pst.setString(7, entity.getLogo());
+
+            if (pst.executeUpdate() == 0) {
+                throw new DaoException("Puede que no se haya guardado.");
+            }
+
+            try (ResultSet generatedKeys = pst.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    entity.setId((int) generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
+
+        } catch (SQLException ex) {
+            throw new DaoException("Error en SQL", ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException ex) {
+                    throw new DaoException("Error en SQL", ex);
+                }
+            }
+        }
     }
 
     @Override
     public void rlUpdate(Empresa entity) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+       PreparedStatement pst = null;
+        try{
+            Calendar calendar = Calendar.getInstance();
+            java.util.Date currentTime = calendar.getTime();
+            long time = currentTime.getTime();
+            
+            pst = (PreparedStatement) conn.prepareStatement(UPDATE);
+            pst.setString(1, entity.getName());
+            pst.setString(2, entity.getAddress());
+            pst.setString(3, entity.getRuc());
+            pst.setString(4, entity.getUbigeo());
+            pst.setString(5, entity.getPhone());
+            pst.setString(6, entity.getEmail());
+            pst.setString(7, entity.getLogo());
+            pst.setLong(8, entity.getId());
 
+            if(pst.executeUpdate() == 0){
+                throw new DaoException("Puede que no se haya modificado.");
+            }
+        }
+        catch (SQLException ex) {
+            throw new DaoException("Error en SQL", ex);
+        }
+        finally{
+            if(pst != null){
+                try{
+                    pst.close();
+                }catch(SQLException ex){
+                    throw new DaoException("Error en SQL", ex);
+                }
+            }
+        }
+    }
     @Override
     public void rlDelete(Empresa entity) throws DaoException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    private Empresa Convert_(ResultSet rs) throws SQLException{
+        Empresa dto = new Empresa();
+        dto.setId(rs.getLong("id"));
+        dto.setName(rs.getString("name"));
+        dto.setAddress(rs.getString("address"));
+        dto.setRuc(rs.getString("ruc"));
+        dto.setUbigeo(rs.getString("ubigeo"));
+        dto.setPhone(rs.getString("phone"));
+        dto.setEmail(rs.getString("enail"));
+        dto.setLogo(rs.getString("logo"));
+        return dto;
+    }
     @Override
     public List<Empresa> findAll(int id) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    PreparedStatement pst = null;
+        ResultSet rs = null;
+        List<Empresa> list = new ArrayList<Empresa>();
+        try{
+            pst = (PreparedStatement) conn.prepareStatement(FINDALL);
+            rs = pst.executeQuery();
+            System.out.println(rs);
+            while(rs.next()){
+                list.add(Convert_(rs));
+            }
+        }
+        catch(SQLException ex){
+            throw new DaoException("Error en SQL", ex);
+        }finally{
+           if(rs != null){
+               try{
+                   rs.close();
+               }
+               catch(SQLException ex){
+                   new DaoException("Error en SQL", ex); 
+               }
+           }
+           if(pst != null){
+               try{
+                   pst.close();
+               }catch(SQLException ex){
+                   new DaoException("Error en SQL", ex);
+               }
+           }
+        }
+        return list;    }
 
     @Override
     public Empresa findById(Empresa entity) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          PreparedStatement pst = null;
+        ResultSet rs = null;
+        Empresa dto = null;
+        try{
+            pst = (PreparedStatement) conn.prepareStatement(FINDBYID);
+            pst.setLong(1, entity.getId());
+            rs = pst.executeQuery();
+            if(rs.next()){
+                dto = Convert_(rs);
+            }else{
+                throw new DaoException("No se ha encontrado el registro");
+            }
+        }
+        catch(SQLException ex){
+            throw new DaoException("Error en SQL", ex);
+        }finally{
+           if(rs != null){
+               try{
+                   rs.close();
+               }
+               catch(SQLException ex){
+                   new DaoException("Error en SQL", ex); 
+               }
+           }
+           if(pst != null){
+               try{
+                   pst.close();
+               }catch(SQLException ex){
+                   new DaoException("Error en SQL", ex);
+               }
+           }
+        }
+        return dto;
     }
-    
+
 }
---
+++ src/WIN30CLC_DAO/MYSQL/MySql_10_DaoUbigeo.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WIN30CLC_DAO.MYSQL;
+
+import WIN30CLC_DAO.DaoException;
+import WIN30CLC_DAO.Dao_10_Ubigeo;
+import WIN31CLC_DTO.Ubigeo;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Carlos Jimenez Gomez
+ */
+    public class MySql_10_DaoUbigeo implements Dao_10_Ubigeo {
+
+    private Connection conn;
+
+    public MySql_10_DaoUbigeo(Connection conn) {
+        this.conn = conn;
+    }
+
+    final String FINDBY_DEP = "SELECT DISTINCT codigo_departamento,descripcion_departamento from UBIGEO WHERE habilitado = 1 order BY descripcion_departamento";
+    final String FINDBYID_DEP_PRO = "SELECT DISTINCT codigo_provincia,descripcion_provincia from UBIGEO where codigo_departamento = ? and habilitado = 1 order BY descripcion_provincia";
+    final String FINDBYID_PRO_DIS = "SELECT distinct codigo_departamento,descripcion_departamento,codigo_provincia,descripcion_provincia,codigo_distrito,descripcion_distrito from UBIGEO  WHERE codigo_departamento = ? and codigo_provincia = ?  AND habilitado = 1 order BY descripcion_distrito ";
+
+    private Ubigeo Convert_Depa(ResultSet rs) throws SQLException {
+        Ubigeo dto = new Ubigeo();
+        dto.setCodigo_departamento(rs.getString("Codigo_departamento"));
+        dto.setDescripcion_departamento(rs.getString("Descripcion_departamento"));
+
+        return dto;
+    }
+
+    @Override
+    public List<Ubigeo> getdepartamentos() throws DaoException {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        List<Ubigeo> list = new ArrayList<Ubigeo>();
+        try {
+            pst = (PreparedStatement) conn.prepareStatement(FINDBY_DEP);
+            rs = pst.executeQuery();
+            System.out.println(rs);
+            while (rs.next()) {
+                list.add(Convert_Depa(rs));
+            }
+        } catch (SQLException ex) {
+            throw new DaoException("Error en SQL", ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+        }
+        return list;
+    }
+
+    private Ubigeo Convert_Depa_Pro(ResultSet rs) throws SQLException {
+        Ubigeo dto = new Ubigeo();
+        dto.setCodigo_provincia(rs.getString("Codigo_provincia"));
+        dto.setDescripcion_provincia(rs.getString("Descripcion_provincia"));
+
+        return dto;
+    }
+
+    @Override
+    public List<Ubigeo> getprovincia(Ubigeo entity) throws DaoException {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        List<Ubigeo> list = new ArrayList<Ubigeo>();
+        try {
+            pst = (PreparedStatement) conn.prepareStatement(FINDBYID_DEP_PRO);
+            pst.setString(1, entity.getCodigo_departamento());
+            rs = pst.executeQuery();
+
+            System.out.println(rs);
+            while (rs.next()) {
+                list.add(Convert_Depa_Pro(rs));
+            }
+        } catch (SQLException ex) {
+            throw new DaoException("Error en SQL", ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+        }
+        return list;
+    }
+
+    private Ubigeo Convert_Prov_Dist(ResultSet rs) throws SQLException {
+        Ubigeo dto = new Ubigeo();
+        dto.setCodigo_distrito(rs.getString("Codigo_distrito"));
+        dto.setDescripcion_distrito(rs.getString("Descripcion_distrito"));
+
+        return dto;
+    }
+
+    @Override
+    public List<Ubigeo> getdistrito(Ubigeo entity) throws DaoException {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        List<Ubigeo> list = new ArrayList<Ubigeo>();
+        try {
+            pst = (PreparedStatement) conn.prepareStatement(FINDBYID_PRO_DIS);
+            pst.setString(1, entity.getCodigo_departamento());
+            pst.setString(2, entity.getCodigo_provincia());
+
+            rs = pst.executeQuery();
+            System.out.println(rs);
+            while (rs.next()) {
+                list.add(Convert_Prov_Dist(rs));
+            }
+        } catch (SQLException ex) {
+            throw new DaoException("Error en SQL", ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException ex) {
+                    new DaoException("Error en SQL", ex);
+                }
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public void rlInsert(Ubigeo entity) throws DaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void rlUpdate(Ubigeo entity) throws DaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void rlDelete(Ubigeo entity) throws DaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Ubigeo> findAll(int id) throws DaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Ubigeo findById(Ubigeo entity) throws DaoException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
