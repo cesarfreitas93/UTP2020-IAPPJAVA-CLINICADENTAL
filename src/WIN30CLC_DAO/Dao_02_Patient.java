031733471c84319ad0efccb69c6c929db2f053e0
cesarFreitas <cesar.freitas.dev@gmail.com>
2020-11-11 22:31:45 -0500

    agregando el metodo insert

--- src/WIN30CLC_DAO/Dao_02_Patient.java
+++ src/WIN30CLC_DAO/Dao_02_Patient.java
@@ -13,4 +13,5 @@ import WIN31CLC_DTO.Patient;
  */
 public interface Dao_02_Patient extends Dao<Patient, Long>{
     public Patient ChekDNI(String dni) throws DaoException;
+    public Patient Insert(Patient entitu) throws DaoException;
 }
